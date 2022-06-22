ebb52b5004a4e57badf501765d620b9cae919e1d
Ghislain Thau <gthau@dalet.com>
2022-06-22 19:39:44 +0200

    added support for typesPrefix and typesSuffix

--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,34 @@ const typesToIgnore = [
   '__Type',
 ];
 
+type Config = {
+  typesPrefix: string;
+  typesSuffix: string;
+}
+
 export const plugin: PluginFunction = (
   schema: GraphQLSchema,
   rawDocuments: Types.DocumentFile[],
   config,
 ) => {
+  const { typesPrefix = '', typesSuffix = '' } = config;
   // keep types in a map, first letter of the typename is always uppercased by the "typescript" plugin
   const types = new Map<string, string>();
   for (const [typeName, namedType] of Object.entries(schema.getTypeMap())) {
     if (isObjectType(namedType) && !typesToIgnore.includes(typeName)) {
-      const name = capitalize(typeName);
-      types.set(name, makeTypeguard(name));
+      const tsTypeName = computeTypeName(typeName, { typesPrefix, typesSuffix });
+      types.set(tsTypeName, makeTypeguard(typeName, tsTypeName));
     }
   }
   return [...types.values()].join('\n\n');
 };
 
-function makeTypeguard(typeName: string) {
-  return `export function is${typeName}(v: any): v is ${typeName} {
-  return v?.__typename === '${typeName}';
+function makeTypeguard(gqlTypeName: string, tsTypeName: string) {
+  return `export function is${tsTypeName}(v: any): v is ${tsTypeName} {
+  return v?.__typename === '${gqlTypeName}';
 }`;
 }
 
-function capitalize(val: string): string {
-  return val.charAt(0).toUpperCase() + val.slice(1);
+function computeTypeName(val: string, { typesPrefix, typesSuffix }: Config): string {
+  return `${typesPrefix}${val}${typesSuffix}`;
 }
