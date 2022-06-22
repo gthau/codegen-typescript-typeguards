a11e0bfdce6390c2ea4d903b338ded5a85b126d3
Ghislain Thau <gthau@dalet.com>
2022-06-22 19:06:04 +0200

    ignore Graphql types such as Directive, Query, Field, etc.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { PluginFunction, Types } from '@graphql-codegen/plugin-helpers';
 import { GraphQLSchema, isObjectType } from 'graphql';
 
+const typesToIgnore = [
+  '__Directive',
+  '__EnumValue',
+  '__Field',
+  '__InputValue',
+  '__Schema',
+  '__Type',
+];
+
 export const plugin: PluginFunction = (
   schema: GraphQLSchema,
   rawDocuments: Types.DocumentFile[],
@@ -9,7 +18,7 @@ export const plugin: PluginFunction = (
   // keep types in a map, first letter of the typename is always uppercased by the "typescript" plugin
   const types = new Map<string, string>();
   for (const [typeName, namedType] of Object.entries(schema.getTypeMap())) {
-    if (isObjectType(namedType)) {
+    if (isObjectType(namedType) && !typesToIgnore.includes(typeName)) {
       const name = capitalize(typeName);
       types.set(name, makeTypeguard(name));
     }
